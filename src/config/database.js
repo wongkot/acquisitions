5c4d33812dce9d221e973d765bea076cbbb16326
Wongkot <wongkot.v@gmail.com>
2026-03-15 20:03:06 +0700

    Fix invalid drizzle import

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 
 const sql = neon(process.env.DATABASE_URL);
 const db = drizzle(sql);
